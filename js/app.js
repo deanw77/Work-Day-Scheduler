875ce0c4cf5a9e9787cb2a6dc0c6da69d29aa250
deanw77 <deanwaring77@outlook.com>
2023-12-02 14:29:11 +0000

    Time blocks added with code to setcolor for past, present & future

--- js/app.js
+++ js/app.js
@@ -20,5 +20,52 @@ const nthNumber = (D) => {
 currentDay.innerHTML = formattedDate + nthNumber(day);
 
 // -----------------------------------------------------------------
+// Add Time Blocks
+const container = document.querySelector(".container");
+const currentHour = currentDate.format('H')
+
+let hourBlock;
+let timeBox;
+let hour = 9;
+let AMPM = "AM";
+let timeBlock;
+let saveBtn;
+
+for (let i = 0; i < 10; i++) {
+    hour = 9;
+    hourBlock = document.createElement("div");
+    hourBlock.classList.add("time-block", "row");
+
+    timeBox = document.createElement("div");
+    timeBox.classList.add("hour");
+    timeBox.setAttribute("style", "width:10%");
+
+    hour = hour + i;
+    let hourCheck = hour;
+    if (hour > 12) hour = hour - 12;
+    if (hour >= 12) AMPM = "PM";
+    timeBox.textContent = hour + AMPM;
+
+    timeBlock = document.createElement("div");
+    timeBlock.setAttribute("style", "width:80%");
+    if (parseInt(currentHour) > hourCheck){
+        timeBlock.classList.add("past", "textarea");
+        
+    } else if (parseInt(currentHour) < hourCheck) {
+        timeBlock.classList.add("future");
+    } else {
+        timeBlock.classList.add("present");
+    }
+
+    saveBtn = document.createElement("div");
+    saveBtn.classList.add("saveBtn");
+    saveBtn.setAttribute("style", "width:10%")
+    saveBtn.textContent = "💾";
+
+    container.append(hourBlock);
+    hourBlock.append(timeBox);
+    hourBlock.append(timeBlock);
+    hourBlock.append(saveBtn);
+}
 
 
