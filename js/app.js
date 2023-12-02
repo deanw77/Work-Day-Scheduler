739d713496190b245a39ee8993b92de936075969
deanw77 <deanwaring77@outlook.com>
2023-12-02 13:35:08 +0000

    Current Date from dayjs retrievedand displayed in the header

--- js/app.js
+++ js/app.js
@@ -1,2 +1,24 @@
+// Display the current date in the header
+const currentDay = document.querySelector("#currentDay");
+
+const currentDate = dayjs(); 
+const formattedDate = currentDate.format('dddd[,] MMMM D'); 
+const day = parseInt(currentDate.format('D'));
+
+// Switch statement to get the correct suffix for date
+const nthNumber = (D) => {
+    if (D > 3 && D < 21) return 'th';
+    switch (D % 10) {
+      case 1:  return "st";
+      case 2:  return "nd";
+      case 3:  return "rd";
+      default: return "th";
+    }
+};
+
+// Display todays date, with the correct suffix to the screen.
+currentDay.innerHTML = formattedDate + nthNumber(day);
+
+// -----------------------------------------------------------------
 
 
